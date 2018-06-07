bc0bf6aa0e663deda929517326bfd162d83b6f31
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-06-06 23:51:54 -0400

    Build collection pages and rename product page creation

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,30 +9,49 @@ const path = require('path')
 exports.createPages = async ({ graphql, boundActionCreators }) => {
     const { createPage } = boundActionCreators
 
-    const pages = await graphql(`
+    const products = await graphql(`
     {
       allShopifyProduct {
         edges {
           node {
             id
-            descriptionHtml
             handle
-            title
           }
         }
       }
     }
   `)
 
-    pages.data.allShopifyProduct.edges.forEach(edge => {
-        console.log(edge.node.id);
-        console.log(edge.node.title)
-        createPage({
-            path: `/${edge.node.handle}`,
-            component: path.resolve('./src/templates/product.js'),
-            context: {
-                id: edge.node.id,
-            },
-        })
+  products.data.allShopifyProduct.edges.forEach(edge => {
+      createPage({
+          path: `/products/${edge.node.handle}`,
+          component: path.resolve('./src/templates/product.js'),
+          context: {
+              id: edge.node.id,
+          },
+      })
+  })
+
+  const collections = await graphql(`
+    {
+      allShopifyCollection {
+        edges {
+          node {
+            id
+            handle
+          }
+        }
+      }
+    }
+  `)
+
+  collections.data.allShopifyCollection.edges.forEach(edge => {
+    createPage({
+      path: `/collections/${edge.node.handle}`,
+      component: path.resolve('./src/templates/collection.js'),
+      context: {
+        id: edge.node.id,
+      },
     })
+  })
 }
