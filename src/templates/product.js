55ecaf37b99d2ae79c96c3a7dbcebeb099d66cce
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-06-06 23:53:50 -0400

    Enhance product template with image and start breadcrumbs
    - TODO: Move breadcrumbs to general component
    - TODO: Fail gracefully for all fields
    - TODO: Add variant support...

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,16 +1,45 @@
 import React from "react";
+import Link from "gatsby-link";
 
 class ProductTemplate extends React.Component {
-    render() {
-        const product = this.props.data.shopifyProduct;
+  render() {
+    const product = this.props.data.shopifyProduct;
 
-        return (
-            <div>
-                <h1>{product.title}</h1>
-                <h2>{product.description}</h2>
-            </div>
-        );
-    }
+    return (
+      <div>
+        {/* TODO: Move to Component */}
+        {true ? (
+          <ol itemscope itemtype="http://schema.org/BreadcrumbList">
+            <li
+              itemprop="itemListElement"
+              itemscope
+              itemtype="http://schema.org/ListItem"
+            >
+              <Link to="/" itemprop="item">
+                <span itemprop="name">Home</span>
+              </Link>
+              <meta itemprop="position" content="1" />
+            </li>
+            <li
+              itemprop="itemListElement"
+              itemscope
+              itemtype="http://schema.org/ListItem"
+            >
+              <span itemprop="name">{product.title}</span>
+              <meta itemprop="position" content="2" />
+            </li>
+          </ol>
+        ) : (
+            ""
+          )}
+        <h1>{product.title}</h1>
+        <h2>{product.description}</h2>
+        {product.images.map((image, i) => {
+          return <img key={i} src={image.originalSrc} alt="" />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default ProductTemplate;
@@ -23,8 +52,31 @@ export const pageQuery = graphql`
             vendor
             tags
             description
+            descriptionHtml
+            images {
+                id
+                originalSrc
+            }
+            extras {
+                minPrice
+                maxPrice
+            }
             productType
+            options {
+                name
+                id
+            }
+            variants {
+                price
+                id
+                title
+                sku
+                availableForSale
+                image {
+                    id
+                    originalSrc
+                }
+            }
         }
     }
-`
-
+`;
