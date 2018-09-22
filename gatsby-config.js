dd5ec1466cf4c0ce131f49839155e2766cdf3914
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-09-21 22:25:39 -0400

    Add back Shopify source plugins (apollo and static)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,38 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Shopify PWA',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-apollo-shopify',
+      options: {
+        shopName: process.env.GATSBY_SHOPIFY_SHOP_NAME,
+        accessToken: process.env.GATSBY_SHOPIFY_ACCESS_TOKEN,
+      },
+    },
+    {
+      resolve: 'gatsby-source-shopify',
+      options: {
+        // The domain name of your Shopify shop. This is required.
+        // Example: 'gatsby-source-shopify-test-shop' if your Shopify address is
+        // 'gatsby-source-shopify-test-shop.myshopify.com'.
+        shopName: process.env.GATSBY_SHOPIFY_SHOP_NAME,
+
+        // An API access token to your Shopify shop. This is required.
+        // You can generate an access token in the "Manage private apps" section
+        // of your shop's Apps settings. In the Storefront API section, be sure
+        // to select "Allow this app to access your storefront data using the
+        // Storefront API".
+        // See: https://help.shopify.com/api/custom-storefronts/storefront-api/getting-started#authentication
+        accessToken: process.env.GATSBY_SHOPIFY_ACCESS_TOKEN,
+
+        // Set verbose to true to display a verbose output on `npm run develop`
+        // or `npm run build`. This prints which nodes are being fetched and how
+        // much time was required to fetch and process the data.
+        // Defaults to true.
+      },
+    },
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
