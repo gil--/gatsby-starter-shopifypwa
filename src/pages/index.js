732c0fef100315a7466403767c145902cd9face5
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-09-23 21:42:18 -0400

    Remove default page-2 and link to all products from homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,7 @@ import { Link } from 'gatsby'
 
 const IndexPage = () => (
   <>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/products/">All Products</Link>
   </>
 )
 
