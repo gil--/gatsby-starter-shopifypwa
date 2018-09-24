a427bf3988374f79db3935d45ee344632d52194f
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-09-24 00:26:55 -0400

    Add product fields needed for pagination

--- src/pages/products.js
+++ src/pages/products.js
@@ -32,6 +32,10 @@ query productsQuery {
                         handle
                     }
                 }
+                pageInfo {
+                    hasNextPage
+                    hasPreviousPage
+                }
             }
         }
     }
