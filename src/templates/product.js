cb8307e90fe8fbf1f2692d42e9f874981d298694
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-09-23 21:39:46 -0400

    Add client-side stock status of product

--- src/templates/product.js
+++ src/templates/product.js
@@ -1,6 +1,27 @@
 import React from "react";
 import { StaticQuery, graphql, Link } from "gatsby"
+import gql from "graphql-tag"
+import { Query } from "react-apollo"
 
+const GET_PRODUCT = gql`
+query($handle: String!) {
+    shop {
+        products(first:1, query: $handle) {
+            edges {
+                node {
+                    variants(first: 1) {
+                        edges {
+                            node {
+                                availableForSale
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+`
 
 class Product extends React.Component {
     render() {
@@ -10,6 +31,21 @@ class Product extends React.Component {
             <div>
                 <h1>{product.title}</h1>
                 <h2>{product.description}</h2>
+                <Query
+                  query={GET_PRODUCT}
+                  variables={{ handle: product.handle }}
+                >
+                    {({ loading, error, data }) => {
+                        if (loading) return <div>Loading...</div>
+                        if (error) return <div>Error :(</div>
+
+                        return (
+                            <>
+                                <h3>Stock Status: {data && data.shop.products && data.shop.products.edges[0].node.variants && data.shop.products.edges[0].node.variants.edges[0].node.availableForSale.toString()}</h3>
+                            </>
+                        )
+                    }}
+                </Query>
                 {product.images && product.images.edges.map((image, i) => {
                     return <img key={i} src={image.node.originalSrc} alt="" />
                 })}
@@ -32,6 +68,7 @@ query($handle: String!) {
             productByHandle(handle: $handle) {
                 title
                 description
+                handle
             }
         }
     }
