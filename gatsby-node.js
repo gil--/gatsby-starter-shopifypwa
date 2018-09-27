339c6d72f5b2b5684682996ece64c07a06eba73b
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-09-26 23:18:05 -0400

    Add new collection template and node logic

--- gatsby-node.js
+++ gatsby-node.js
@@ -9,6 +9,9 @@ const path = require('path')
 exports.createPages = async ({ graphql, actions }) => {
     const { createPage } = actions
 
+    /**********************
+        PRODUCTS
+    ***********************/
     let allProducts = []
     const productLimitPerQuery = 250;
 
@@ -78,7 +81,7 @@ exports.createPages = async ({ graphql, actions }) => {
         await getMoreProducts(currentCursor = productsCache.data.shopify.shop.products.edges[productsCache.data.shopify.shop.products.edges.length - 1].cursor)
     }
 
-    allProducts.forEach(product => {
+    allProducts && allProducts.forEach(product => {
         createPage({
             path: `/products/${product.node.handle}`,
             component: path.resolve('./src/templates/product.js'),
@@ -88,26 +91,32 @@ exports.createPages = async ({ graphql, actions }) => {
         })
     })
 
-//     const collections = await graphql(`
-//     {
-//       allShopifyCollection {
-//         edges {
-//           node {
-//             id
-//             handle
-//           }
-//         }
-//       }
-//     }
-//   `)
+    /**********************
+        COLLECTIONS
+    ***********************/
+    const allCollections = await graphql(`
+    {
+        shopify {
+            shop {
+                collections(first: 250) {
+                    edges {
+                        node {
+                            handle
+                        }
+                    }
+                }
+            }
+        }
+    }
+  `)
 
-//     collections.data.allShopifyCollection.edges.forEach(edge => {
-//         createPage({
-//             path: `/collections/${edge.node.handle}`,
-//             component: path.resolve('./src/templates/collection.js'),
-//             context: {
-//                 id: edge.node.id,
-//             },
-//         })
-//     })
+    allCollections && allCollections.data.shopify.shop.collections.edges.forEach(edge => {
+        createPage({
+            path: `/collections/${edge.node.handle}`,
+            component: path.resolve('./src/templates/collection.js'),
+            context: {
+                handle: edge.node.handle,
+            },
+        })
+    })
 }
---
+++ src/templates/collection.js
@@ -0,0 +1,62 @@
+import React from "react";
+import { graphql } from "gatsby"
+import ProductList from '../components/ProductList'
+
+class Collection extends React.Component {
+    state = { }
+
+    render() {
+        const { title, description, products } = this.props.data.shopify.shop.collectionByHandle
+
+        return (
+            <>
+                <h1>{title}</h1>
+                <div>{description}</div>
+                <ProductList products={products} />
+            </>
+        )
+    }
+}
+
+export default Collection
+
+export const query = graphql`
+query ($handle: String!) {
+    shopify {
+        shop {
+            collectionByHandle(handle: $handle) {
+                handle
+                title
+                description
+                image {
+                    originalSrc
+                    altText
+                }
+                products(first: 20) {
+                    edges {
+                        node {
+                            id
+                            handle
+                            title
+                            priceRange {
+                                minVariantPrice {
+                                    currencyCode
+                                    amount
+                                }
+                            }
+                            images(first: 2) {
+                                edges {
+                                    node {
+                                        originalSrc
+                                        altText
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+`;
