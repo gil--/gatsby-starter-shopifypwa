edffba7347a613dde20d689343e80397c14edb32
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-09-29 23:40:52 -0400

    Add forgot password page
    This page will initiate the forgot password email
    
    ```diff
    -<td class="button__cell"><a href="{{customer.reset_password_url}}" class="button__text">Reset your password</a></td>
    +{% assign url_parts = customer.reset_password_url  | split: '/' %}
    +<td class="button__cell"><a href="{{shop.url}}/account/reset?id={{url_parts[5]}}&token={{url_parts[6]}}" class="button__text">Reset your password</a></td>
    ```

---
+++ src/pages/account/forgotpassword.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo'
+import { Link, navigate } from 'gatsby'
+import ContextConsumer from '../../layouts/context'
+import AuthenticationWrapper from '../../components/account/AuthenticationWrapper'
+
+const CUSTOMER_RESET = gql`
+mutation customerRecover($email: String!) {
+    customerRecover(email: $email) {
+        userErrors {
+            field
+            message
+        }
+    }
+}
+
+`
+
+class ForgotPassword extends React.Component {
+    state = {
+        email: '',
+    }
+
+    handleEmailChange = e => {
+        e.preventDefault()
+
+        this.setState({
+            email: e.target.value,
+        });
+    }
+
+    render() {
+        const pageContent = (
+            <ContextConsumer>
+                {({ set }) => {
+                    return <>
+                        <h1>Forgot Your Password</h1>
+                        <form>
+                            <ul>
+                                <li>
+                                    <label htmlFor="forgotEmail">Email</label>
+                                    <input id="forgotEmail" type="email" value={this.state.email} onChange={this.handleEmailChange} required="" />
+                                </li>
+                            </ul>
+                            <Mutation
+                                mutation={CUSTOMER_RESET}
+                                onCompleted={data => {
+                                    if (data.customerRecover.userErrors.length || data.customerAccessTokenCreate.customerUserErrors.length) {
+                                        return
+                                    }
+
+                                    navigate('/account/login')
+                                }}
+                            >
+                                {(forgotPassword, { loading }) => {
+                                    if (loading) return <button disabled="disabled">Submitting Request...</button>
+
+                                    return (
+                                        <button
+                                            onClick={e => {
+                                                e.preventDefault()
+
+                                                if (!this.state.email) {
+                                                    return
+                                                }
+
+                                                forgotPassword({
+                                                    variables: {
+                                                        "email": this.state.email,
+                                                    }
+                                                })
+                                            }}
+                                        >Reset Password</button>
+                                    )
+                                }}
+                            </Mutation>
+                        </form>
+                        <Link to={`account/login`}>Log In</Link>
+                    </>
+                }}
+            </ContextConsumer>
+        )
+
+        return (
+            <AuthenticationWrapper
+                navigate={`/account`}
+                false={pageContent}
+            />
+        )
+    }
+}
+
+export default ForgotPassword
