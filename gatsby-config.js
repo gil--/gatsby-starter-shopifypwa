46a50298bd1d6f453443bd1d08e96bb227c20376
Gil Greenberg <ggreenberg@somethingdigital.com>
2018-10-15 18:03:49 -0400

    update manifest name and make standalone

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,12 +30,12 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
+        name: 'Shopify PWA',
+        short_name: 'ShopifyPWA',
         start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
+        background_color: '#50B83C',
+        theme_color: '#50B83C',
+        display: 'standalone',
         icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
       },
     },
